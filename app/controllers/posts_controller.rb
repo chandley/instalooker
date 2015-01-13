9d483b4ed08a03da25131e4203ed2dc226b42790
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-13 16:00:21 +0000

    AWS S3 storage working (local enviroment variables)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-
+    @posts = Post.all
   end
 
   def new
@@ -9,7 +9,13 @@ class PostsController < ApplicationController
   end
 
   def create
+    Post.create(post_params)
     redirect_to '/'
+  end
+
+  def post_params
+    #permit post?
+    params.require(:post).permit(:caption, :picture)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
+  has_attached_file :picture,
+    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+    :storage => :s3,
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['WS_SECRET_ACCESS_KEY']
+    }
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\z/
   
 end
